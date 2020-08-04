9ab62fa78d50759e18c7f4c428c4319a0d61fff6
Nicole Antonacci <nicole.antonacci@gmail.com>
2020-08-04 12:31:01 -0700

    Changed join styles and fixed join submit.

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, withRouter } from 'react-router-dom';
 
 import './Join.css';
 
-const Join = () => {
+const Join  = withRouter(({ history }) => {
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
 
+    const submitHandler = (event) => {
+        if (!name || !room){
+            event.preventDefault()
+        } else {
+            history.push(`/chat?name=${name}&room=${room}`)
+        }
+    }
+
+
     return(
         <div className="joinOuterContainer">
-            <div className="joinInnerContainer">
+            <form className="joinInnerContainer" onSubmit={submitHandler}>
                 <h1 className="heading">Join</h1>
                 <div>
                     <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value) } />
@@ -20,12 +29,13 @@ const Join = () => {
                     <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value) } />
                 </div>
 
-                <Link onClick={(event) => (!name || !room) ? event.preventDefault : null} to={`/chat?name=${name}&room=${room}`}>
+                {/* <Link onClick={(event) => (!name || !room) ? event.preventDefault : null} to={`/chat?name=${name}&room=${room}`}> */}
                     <button className="button mt-20" type="submit">sign in</button>
-                </Link>
-            </div>
+                {/* </Link> */}
+            </form>
         </div>
     )
 }
+)
 
 export default Join;
