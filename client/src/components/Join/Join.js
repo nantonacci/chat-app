55ccdd493521688e0b042945758a271a730d5fea
Nicole Antonacci <nicole.antonacci@gmail.com>
2020-08-05 14:08:08 -0700

    Styling for color circles lookin good.

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -29,43 +29,36 @@ const Join  = withRouter(({ history }) => {
                     <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value) } />
                 </div>
 
-                <div className="radios">
-                    <div id="softlime">
-                        <input type="radio" id="softlime" name="color" value="softlime" />
-                        <label for="softlime"></label>
+                <div className="colors">
+                    <div className="circles" id="softlime">
+                        
                     </div>
 
-                    <div id="seablue">
-                        <input type="radio" id="seablue" name="color" value="seablue"/>
-                        <label for="seablue"></label>
+                    <div className="circles" id="seablue">
+                        
                     </div>
 
-                    <div id="orange">
-                        <input type="radio" id="orange" name="color" value="orange"/>
-                        <label for="orange"></label>
+                    <div className="circles" id="orange">
+                        
                     </div>
 
-                    <div id="magenta">
-                        <input type="radio" id="magenta" name="color" value="magenta"/>
-                        <label for="magenta"></label>
+                    <div className="circles" id="magenta">
+                        
                     </div>
 
-                    <div id="redorange">
-                        <input type="radio" id="redorange" name="color" value="redorange"/>
-                        <label for="redorange"></label>
+                    <div className="circles" id="redorange">
+                        
                     </div>
 
-                    <div id="yellow">
-                        <input type="radio" id="yellow" name="color" value="yellow" />
-                        <label for="yellow"></label>
+                    <div className="circles" id="yellow">
+                        
                     </div>
 
-                    <div id="deepblue">
-                        <input type="radio" id="deepblue" name="color" value="deepblue"/>
-                        <label for="deepblue" ></label>
+                    <div className="circles" id="deepblue">
+                        
                     </div>
                 </div>
-                {/* end radios */}
+                {/* end colors */}
 
                     <button id="color" className="button mt-20" type="submit">sign in</button>
             </form>
